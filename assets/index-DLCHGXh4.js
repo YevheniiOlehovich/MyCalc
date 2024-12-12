cf28f77790ed052d0ee0f62c6a35cc3310d1a803
Yevhenii Khyzhniak <keildra258@gmail.com>
2024-12-12 23:33:29 +0200

    Updates

--- assets/index-DLCHGXh4.js
+++ assets/index-DLCHGXh4.js
@@ -130,7 +130,7 @@ Error generating stack: `+u.message+`
             transform: translateX(0);
             justify-content: space-between;
             /* height: calc(100vh - 8px); */
-            height: 700px;
+            height: 500px;
             width: 100vw;
         } 
     `,Shadow:Ne.div`
